db979c4bf21b9867497956b2248bf92f8c706637
Emmanuel Alabi <bobslegend795@gmail.com>
2023-02-17 13:01:38 -0800

    Base61

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,9 +8,12 @@
 
 int main(void)
 {
+	int n;
 	char ch;
 
-	for (ch = 'z'; ch >= 'a'; ch--)
+	for (n = 48; n < 58; n++)
+		putchar(n);
+	for (ch = 'a'; ch <= 'f'; ch++)
 		putchar(ch);
 	putchar('\n');
 	return (0);
