4e142f8fc2093bf37273c0005f7a6ed436138906
Emmanuel Alabi <bobslegend795@gmail.com>
2023-03-20 18:17:57 -0700

    done

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,14 +1,14 @@
 #include "dog.h"
 
 /**
-* init_dog - Function to initialize a struct
-* @d: Struct to initialize
-* @name: The name to give the dog struct
-* @age: The age to give the struct dog
-* @owner: The name of the owner of the struct dog
-*
-* Return: void
-*/
+ * init_dog - Function to initialize a struct
+ * @d: Struct to initialize
+ * @name: The name to give the dog struct
+ * @age: The age to give the struct dog
+ * @owner: The name of the owner of the struct dog
+ *
+ * Return: void
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if(d)
