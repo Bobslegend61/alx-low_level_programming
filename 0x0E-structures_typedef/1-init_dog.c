8833d419060ac78d9ec2821664326cbc165c7106
Emmanuel Alabi <bobslegend795@gmail.com>
2023-03-20 18:16:37 -0700

    done

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -6,6 +6,8 @@
 * @name: The name to give the dog struct
 * @age: The age to give the struct dog
 * @owner: The name of the owner of the struct dog
+*
+* Return: void
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
