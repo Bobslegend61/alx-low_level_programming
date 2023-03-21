29866e856624634c4a66a139e954ab2b4d40e94a
Emmanuel Alabi <bobslegend795@gmail.com>
2023-03-20 17:50:12 -0700

    done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,15 +12,14 @@
 struct dog
 {
 	char* name;
-	char* owner;
 	float age;
+	char* owner;
 };
 
 /**
 * dog_t - Typedef for struct dog 
 */
 typedef struct dog dog_t;
-
 
 void init_dog(struct dog*, char*, float, char*);
 void print_dog(struct dog*);
