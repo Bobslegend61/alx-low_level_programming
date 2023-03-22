b40791d9e36105c95a7941b0062459da26b82d0a
Emmanuel Alabi <bobslegend795@gmail.com>
2023-03-22 15:42:47 -0700

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,13 @@
-  1 #include "function_pointers.h"
-  2 
-  3 /**
-  4   * print_name - Prints a name
-  5   * @name: Name to print
-  6   * @f: Function to define print format
-  7   *
-  8   * Return: Nothing
-  9   */
- 10 void print_name(char *name, void (*f)(char *))
- 11 {
- 12         f(name);
- 13 }      
+#include "function_pointers.h"
+
+/**
+  * print_name - Prints a name
+  * @name: Name to print
+  * @f: Function to define print format
+  *
+  * Return: Nothing
+  */
+void print_name(char *name, void (*f)(char *))
+{
+        f(name);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
-  1 #ifndef _FUNCTION_POINTERS_H_
-  2 #define _FUNCTION_POINTERS_H_
-  3 
-  4 int _putchar(char c);
-  5 void print_name(char *name, void (*f)(char *));
-  6 
-  7 #endif /* _FUNCTION_POINTERS_H */
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+#endif /* _FUNCTION_POINTERS_H */
