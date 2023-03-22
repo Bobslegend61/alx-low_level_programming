cb3719f56767c5f406beae3a215d051e12e7cf3e
Emmanuel Alabi <bobslegend795@gmail.com>
2023-03-22 16:50:58 -0700

    we

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,11 +10,9 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-        if (array && size && action)
-        {
-                unsigned int i;
+	size_t i;
 
+        if (array && size && action)
                 for (i = 0; i < size; i++)
                         action(*(array + i));
-        }
 }
