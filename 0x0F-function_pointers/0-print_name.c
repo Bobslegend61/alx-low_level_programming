fb1850a8caa09300395716a3bff18b2e5d27b363
Emmanuel Alabi <bobslegend795@gmail.com>
2023-03-22 15:55:12 -0700

    done

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,11 @@
 #include "function_pointers.h"
 
 /**
-  * print_name - Prints a name
+  * print_name - prints a name
   * @name: name to print
   * @f: function to define print format
   *
-  * Return: Nothing
+  * Return: nothing
   */
 void print_name(char *name, void (*f)(char *))
 {
