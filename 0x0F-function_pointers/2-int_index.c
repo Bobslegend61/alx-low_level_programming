55fbbeee63da70f73c68f542018ab7fdcd6488f0
Emmanuel Alabi <bobslegend795@gmail.com>
2023-03-22 17:48:59 -0700

    as

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,13 +12,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int res = -1, i;
 
-	if (!size || size <= 0)
-		return (-1);
-	for (i = 0; i < size; i++)
-		if (cmp(array[i]))
-		{
-			res = i;
-			break;
-		}
+	if (array && cmp)
+	{
+		if (!size || size <= 0)
+			return (-1);
+		for (i = 0; i < size; i++)
+			if (cmp(array[i]))
+			{
+				res = i;
+				break;
+			}
+	}
 	return res <= 0 ? -1 : res;
 }
