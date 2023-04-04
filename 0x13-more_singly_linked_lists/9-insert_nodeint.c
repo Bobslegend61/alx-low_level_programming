7086a4acc84f6ad11f1ff5f04d2a59eac1e46972
Emmanuel Alabi <bobslegend795@gmail.com>
2023-04-04 09:42:02 -0700

    i

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new->next = *head;
 		*head = new;
-		return (1);
+		return (new);
 	}
 	while (i < (idx - 1) && *head)
 	{
