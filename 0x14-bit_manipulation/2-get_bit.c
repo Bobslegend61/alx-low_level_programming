ef689c4663cb0f8c8f6fec910c04768e79162ff5
Emmanuel Alabi <bobslegend795@gmail.com>
2023-04-10 05:03:25 -0700

    rr

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,7 +11,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int s = sizeof(unsigned long int);
 
-	if (index < 0 || index > ((s * 8) - 1))
+	if (index > ((s * 8) - 1))
 		return (-1);
 	return ((n >> index) & 1);
 }
