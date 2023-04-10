20933a255b893be13f92161e05782cb993abf25d
Emmanuel Alabi <bobslegend795@gmail.com>
2023-04-10 04:54:11 -0700

    ff

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -11,7 +11,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int m;
 
-	if (index < 0 || index > 63)
+	if (index > 63)
 		return (-1);
 	m = ((*n >> index) & 1);
 	if (m == 1)
