9a335bdbde3b80e5ee5a9aa6dabcc4eb5735ab1b
Emmanuel Alabi <bobslegend795@gmail.com>
2023-04-13 15:59:04 -0700

    ff

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,6 +16,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
+	if (!text_content)
+		return (1);
 	w_count = write(fd, text_content, (sizeof(char) * strlen(text_content)));
 	if (w_count == -1)
 		return (-1);
